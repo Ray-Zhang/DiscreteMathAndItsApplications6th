8a2f2b319c8a048d3a0797be6a21d39188c3f213
Rui Zhang <zryaq1983@gmail.com>
2017-08-08 04:38:37 +0900

    change code applied to any size of power

--- chapter1.LogicAndProof/ComputationAndExplorations/exercise01.cc
+++ chapter1.LogicAndProof/ComputationAndExplorations/exercise01.cc
@@ -11,39 +11,41 @@
 #include <iostream>
 #include <cmath>
 
+const int POWER_SIZE = 3;
+
 /*
  * look for positive integers that are the sum of squares of four different positive integers
  */
 void FindExponetiationPermutation(const int n)
 {
-    int max_possible_i = std::sqrt(n);
-    int min_possible_i = std::sqrt(n / 4);
+    int max_possible_i = std::pow(n, 1.0/POWER_SIZE);
+    int min_possible_i = std::pow(n / 4, 1.0/ POWER_SIZE);
     for (int i = max_possible_i; i > min_possible_i; i--)
     {
         int remain = n;
-        remain -= static_cast<int>(std::pow(i, 2));
-        int max_possible_j = i - 1 >= std::sqrt(remain) ? std::sqrt(remain) : i - 1;
-        int min_possible_j = std::sqrt(remain / 3);
+        remain -= static_cast<int>(std::pow(i, POWER_SIZE));
+        int max_possible_j = i - 1 >= std::pow(remain, 1.0/POWER_SIZE) ? std::pow(remain, 1.0/POWER_SIZE) : (i - 1);
+        int min_possible_j = std::pow(remain / 3, 1.0/POWER_SIZE);
         for (int j = max_possible_j; j > min_possible_j; j--)
         {
-            remain -= static_cast<int>(std::pow(j, 2));
+            remain -= static_cast<int>(std::pow(j, POWER_SIZE));
             if (remain <= 0) break;
-            int max_possible_k = j - 1 >= std::sqrt(remain) ? std::sqrt(remain) : j - 1;
-            int min_possible_k = std::sqrt(remain / 2);
+            int max_possible_k = j - 1 >= std::pow(remain, 1.0/POWER_SIZE) ? std::pow(remain, 1.0/POWER_SIZE) : (j - 1);
+            int min_possible_k = std::pow(remain / 2, 1.0/POWER_SIZE);
             for (int k = max_possible_k; k > min_possible_k; k--)
             {
-                remain -= static_cast<int>(std::pow(k, 2));
+                remain -= static_cast<int>(std::pow(k, POWER_SIZE));
                 if (remain <= 0) break;
-                int max_possible_l = k - 1 >= std::sqrt(remain) ? std::sqrt(remain) : k - 1;
+                int max_possible_l = k - 1 >= std::pow(remain, 1.0/POWER_SIZE) ? std::pow(remain, 1.0/POWER_SIZE) : (k - 1);
                 for (int l = max_possible_l;;l--)
                 {
-                    if (remain == static_cast<int>(std::pow(l, 2)))
+                    if (remain == static_cast<int>(std::pow(l, POWER_SIZE)))
                     {
                         std::cout << "Found for " << n << std::endl;
                         std::cout << "i: " << i << " j: " << j << " k: " << k << " l: " << l << std::endl;
                         return;
                     }
-                    else if (remain > static_cast<int>(std::pow(l, 2)))
+                    else if (remain > static_cast<int>(std::pow(l, POWER_SIZE)))
                     {
                         break;
                     }
